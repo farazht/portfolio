0d303aa2e441199cfacb36a855f84178860ad608
Faraz Hosseinian-Tehrani <107619942+farazht@users.noreply.github.com>
2023-07-19 22:12:49 -0700

    Initial commit

---
+++ scripts/typing-animation.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing-animation", {
+    strings: ["a student", "a creator", "an innovator", "a learner", "a developer", "a thinker", "a maker", "a programmer"],
+    typeSpeed: 150,
+    backSpeed: 150,
+    loop: true
+});
