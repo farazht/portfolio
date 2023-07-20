f1af174ee5c4f21079bef02e1c95de7c162521ee
Faraz Hosseinian-Tehrani <107619942+farazht@users.noreply.github.com>
2023-07-20 00:59:17 -0700

    Delete scripts directory

--- scripts/typing-animation.js
+++
@@ -1,6 +0,0 @@
-var typed = new Typed(".typing-animation", {
-    strings: ["a student", "a creator", "an innovator", "a learner", "a developer", "a thinker", "a maker", "a programmer"],
-    typeSpeed: 150,
-    backSpeed: 150,
-    loop: true
-});
