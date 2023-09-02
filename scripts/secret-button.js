5d0a838426ce73ec490ecc9ae205281cd3a2cf36
farazht <tehranifaraz314@gmail.com>
2023-09-01 20:16:00 -0600

    Update secret button

--- scripts/secret-button.js
+++ scripts/secret-button.js
@@ -2,12 +2,12 @@ const button = document.getElementById('secret-button');
 let currentScale = 1;
 
 button.addEventListener('click', function() {
-    if (currentScale > 500) {
-        window.location.href = 'secret-page.html';
-    }
-    else {
-        currentScale *= 1.5;
-        this.style.transform = `scale(${currentScale})`;
-        console.log(currentScale);
-    }
+    setInterval(function() {
+        currentScale *= 1.02;
+        button.style.transform = `scale(${currentScale})`;
+        if (currentScale > 1000) {
+            window.location = 'secret-page.html';
+        }
+    } , 10);
 });
+
