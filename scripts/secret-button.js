f08bf149a270442acd0818e1e8f381841819e2c8
farazht <tehranifaraz314@gmail.com>
2023-09-05 19:44:41 -0700

    Fixed secret page bug

--- scripts/secret-button.js
+++ scripts/secret-button.js
@@ -1,13 +1,15 @@
 const button = document.getElementById('secret-button');
 let currentScale = 1;
+let grow;
 
 button.addEventListener('click', function() {
-    setInterval(function() {
+    grow = setInterval(function() {
         if (currentScale < 1000) {
             currentScale *= 1.02;
-            button.style.transform = `scale(${currentScale})`;    
+            button.style.transform = `scale(${currentScale})`;
         } else {
             window.location = 'secret-page.html';
+            clearInterval(grow);
         }
-    } , 10);
+    }, 10);
 });
